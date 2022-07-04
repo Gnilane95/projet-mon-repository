edf15a8c2b0ba9f18db3a926bfb089cbf5ee9d91
Gnilane95 <agathegnilanediouf@gmail.com>
2022-07-04 16:25:31 +0200

    hi

--- src/components/about-page/CardDevFull.js
+++ src/components/about-page/CardDevFull.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function CardRight() {
   return (
     <div className='pl-0'>
-        <h1 className='tex'>Justin Bieber</h1>
+        <h1 className='text-4xl'>Justin Bieber</h1>
         <h2>Description</h2>
     </div>
   )
